804020099e482bb1744f396fa84c4629ce2b9aa2
Nazma1997 <nazma.ist.707@gmail.com>
2022-06-04 17:48:21 +0600

    added

--- express-js/app/app.js
+++ express-js/app/app.js
@@ -1,34 +1,20 @@
 require('dotenv').config('../.env');
 const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
- 
+const {errorHandler, notFoundHandler} = require('./error')
 
 const app = express();
-app.use([
-  morgan('dev'), 
-  cors(),
-  express.json()
-])
 
-app.get('/health', (_req, res) => {
-  res.json({message: "Success"});
-});
+// middleware 
+app.use(require('./middleware'));
 
-// Custom Error Handler 
-app.use((_req, _res, next) => {
-  const error = new Error('Resource not found');
-  error.status = 404;
-  next(error)
-})
+// router 
+app.use(require('./route'));
+app.use(notFoundHandler);
+app.use(errorHandler);
 
-// Global error handler 
-app.use((error, _req, res, _next) => {
-  if(error.status){
-      return res.status(error.status).json({message: error.message})
-  }
-  res.status(500).json({message: "something went wrong"})
-})
+
+
+
 
 
 module.exports = app;
