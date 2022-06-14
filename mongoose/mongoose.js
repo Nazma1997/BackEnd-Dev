79875018ad62e9a722939d7e16b35856847f5367
Nazma1997 <nazma.ist.707@gmail.com>
2022-06-14 15:07:34 +0600

    added

--- mongoose/mongoose.js
+++ mongoose/mongoose.js
@@ -1,10 +1,26 @@
 const mongoose = require('mongoose');
 
+const peopleScheme = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email:String,
+  age: Number
+})
 
+const People = mongoose.model('People', peopleScheme)
+
+async function personCreate(data){
+  const person = new People({...data});
+  await person.save()
+  return person
+}
 mongoose
 .connect('mongodb://localhost:27017/demo')
-.then(() => {
+.then(async () => {
   console.log('Database is Connected')
+  await personCreate({firstName: 'Tanbir', lastName:'Ahamed', email:'tanbit@gmail.com', age:34})
+  console.log('person created')
+ 
 })
 .catch((error) => {
   console.log(error)
